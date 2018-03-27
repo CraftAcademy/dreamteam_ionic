2c7cecda9dd82305d1da378b64a25ac383b81f27
bollen <david.boll@me.com>
2018-03-27 16:00:25 +0200

    Adds inital test for home

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -10,7 +10,11 @@ export class Page {
     return browser.getTitle();
   }
 
- getPageOneTitleText() {
-    return element(by.tagName('page-one')).element(by.tagName('ion-title')).getText();
-  }
+//  getPageTitleText(name) {
+//     return element(by.tagName('page-' + name)).element(by.tagName('ion-title')).getText();
+//   }
+
+getPageTitleText(name) {
+  return element(by.tagName('page-' + name)).element(by.tagName('ion-title')).getText();
+}
 }
