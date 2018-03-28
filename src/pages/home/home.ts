cd1ea02e2e1a3ea8b7e1f800db9d1df4d7efd95d
bollen <david.boll@me.com>
2018-03-28 17:27:41 +0200

    Connects to API Fetches title and body from external API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,7 +4,7 @@ import { Http, Response } from '@angular/http';
 import { HttpModule } from '@angular/http';
 import { resolvePtr } from 'dns';
 import 'rxjs/add/operator/map';
-import { HttpHeaders } from '@angular/common/http';
+//import { HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -23,15 +23,17 @@ export class HomePage {
 
   private apiUrl: string = 'http://localhost:3000/api/v0/articles';
   // private apiUrl: string = 'https://dreamteam-publications.herokuapp.com/api/v0/articles';
-  data = {};
+  articles: any[];
+  categories: any[];
 
   constructor(private http: Http, navCtrl: NavController) {
     // Get some articles from API and store them in data
   
     this.http.get(this.apiUrl)
              .map((response: Response) => response.json())
-             .subscribe(data => {
-                console.log(data);
+             .subscribe(articles => {
+                console.log(articles);
+                this.articles = articles.data;
              });
   }
 
