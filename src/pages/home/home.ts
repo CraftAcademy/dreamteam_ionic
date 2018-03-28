0726c097317ac3a48e355a5faff1739879cdb671
bollen <david.boll@me.com>
2018-03-28 15:26:32 +0200

    Adds connection to localhost-api for testing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Response } from '@angular/http';
+import { HttpModule } from '@angular/http';
+import { resolvePtr } from 'dns';
+import 'rxjs/add/operator/map';
+import { HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   obj = {
-    "title": "My amazing article",
-    "content": "Lorem ipsum dolor amet readymade gluten-free shoreditch..",
-    "image": "../assets/imgs/article-image-mock.jpg",
-    "categories": "Fashion, Tech, Sports"
-  }
+    title: "My amazing article",
+    content: "Lorem ipsum dolor amet readymade gluten-free shoreditch..",
+    image: "../assets/imgs/article-image-mock.jpg",
+    categories: "Fashion, Tech, Sports"
+  };
 
-  constructor(public navCtrl: NavController) {
+  private apiUrl: string = 'http://localhost:3000/api/v0/articles';
+  // private apiUrl: string = 'https://dreamteam-publications.herokuapp.com/api/v0/articles';
+  data = {};
 
+  constructor(private http: Http, navCtrl: NavController) {
+    // Get some articles from API and store them in data
+  
+    this.http.get(this.apiUrl)
+             .map((response: Response) => response.json())
+             .subscribe(data => {
+                console.log(data);
+             });
   }
 
 }
